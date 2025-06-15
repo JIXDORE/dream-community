7f52374ef80c92000b4eb560160b06ebf3db8705
JIXDORE <tkumpliw@gmail.com>
2025-06-15 15:13:19 +0700

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,9 +1,1 @@
-export const firebaseConfig = {
-  apiKey: "AIzaSyCQ5EtSLiHFfrfKA4fBF3mOs9LyofJmzTk",
-  authDomain: "dream-community-f781a.firebaseapp.com",
-  projectId: "dream-community-f781a",
-  storageBucket: "dream-community-f781a.appspot.com",
-  messagingSenderId: "955028911393",
-  appId: "1:955028911393:web:d11ebe3f88985254f2fa60",
-  measurementId: "G-814HMHPVD3"
-};
+
